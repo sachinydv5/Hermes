e29b06cf49637ab01e7e38154a3bb4495e411f6a
yogeshk4124 <yogeshk4124@gmail.com>
2024-11-24 00:20:57 +0530

    error response added

--- Hermes-Backend/src/controllers/product.controller.ts
+++ Hermes-Backend/src/controllers/product.controller.ts
@@ -1,18 +1,16 @@
-import { TriggerOtpResponse } from "../types/auth/trigger";
 import { ProductRequestSchema, ProductResponseSchema } from "../types/product/product";
 import { TypedRequest, TypedResponse } from "../types/express.types";
 import { addProductInDB } from "../database/product/product";
 
 export const addProduct = async (req: TypedRequest<ProductRequestSchema>, res: TypedResponse<ProductResponseSchema>) => {
     try {
-        console.log("Here");
         // validateCategory();
         // validateCollectionId();
         // validatePickupAddress();
         addProductInDB(req.body);
         res.json({status:"PRODUCT_ADDED_SUCCESSFULLY"})
     } catch(error){
-
+        res.json({ error_code: "INTERNAL_SERVER_ERROR", description: "Some error Occurred" });
     }
 
 }
