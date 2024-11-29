cf1e0c75a9a7fd31de8d653015bf21775d7e3b6e
sachinyadav2328 <ydvsachin14@gmail.com>
2024-11-29 14:11:52 +0530

    kuch to kiya hai

--- Hermes-Frontend/src/utils/AppRoutes.ts
+++ Hermes-Frontend/src/utils/AppRoutes.ts
@@ -5,10 +5,6 @@ export const AppRoutes = {
     HOME: '/home',
     CREATINGLIST:'/creatinglist',
     CREATESINGLE:'/createsingle',
-<<<<<<< HEAD
     MARKET:'/market',
     ProductList:'/productlist'
-=======
-    // SIGNUPFORM:'/signup'
->>>>>>> a6236d1bf706187324308cb8c0a8698fad49afe6
 }
