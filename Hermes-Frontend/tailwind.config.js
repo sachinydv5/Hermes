abc040a17cd7f6921e4ac55477d5ca09e56b8886
Shreyans Jain <shreyans1313@gmail.com>
2024-12-15 18:23:57 +0530

    🚀 deploy

--- Hermes-Frontend/tailwind.config.js
+++ Hermes-Frontend/tailwind.config.js
@@ -1,3 +1,6 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
@@ -57,5 +60,5 @@ export default {
     		}
     	}
     },
-	plugins: [require("tailwindcss-animate")],
+	plugins: [tailwindcssAnimate],
   }
